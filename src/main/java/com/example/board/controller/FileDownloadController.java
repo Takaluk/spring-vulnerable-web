f068d23336e8ac45e1e3cc17cf897bc1d6c88eb1
yslee20 <ingress0419@gmail.com>
2025-01-03 11:50:08 +0900

    fix: edit download logic to vulnerable

--- src/main/java/com/example/board/controller/FileDownloadController.java
+++ src/main/java/com/example/board/controller/FileDownloadController.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
@@ -16,9 +16,9 @@ import java.nio.file.Files;
 @RestController
 public class FileDownloadController {
 
-    // 경로 추적 취약점이 존재하는 코드
-    @GetMapping("/files/uploads/{fileName}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) throws IOException {
+    // 파일 다운로드 요청 처리 (경로 추적 취약점 포함)
+    @GetMapping("/files/uploads")
+    public ResponseEntity<Resource> downloadFile(@RequestParam String fileName) throws IOException {
         // 사용자의 입력을 그대로 경로에 사용 (경로 추적 취약점 발생 가능)
         File file = new File("uploads/" + fileName);  
 
